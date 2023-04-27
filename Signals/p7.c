82398c8975a58f547f6d9327fca0e804c42affc9
jagarwa1 <jaiveer.agarwal@gmail.com>
2023-04-26 23:00:43 -0400

    project 7 update

--- Signals/p7.c
+++ Signals/p7.c
@@ -13,6 +13,10 @@ Known Bugs     -
 #include <unistd.h>
 #include <time.h>
 
+
+// child operation
+void spawnSub(char *Terminal);
+
 int main (int argc, char* arv[]) {
 
     FILE* fpt;
@@ -24,39 +28,71 @@ int main (int argc, char* arv[]) {
     time_t t;
     time(&t);
 
-    // printf("i'm parent number %d\n",getpid());
+    // store this current tty as the parent tty    
+    char parentTTY[15];
+    strcpy(parentTTY, ttyname(STDOUT_FILENO));
+
 
     // get opened terminals
     for (int i = 0; i <= 136; i++) {
         sprintf(temp, "/dev/pts/%d", i);
-        if (fopen(temp, "r") != NULL) {
+        if (isatty(temp)) {
             availableTerms[itemCount] = (char*)malloc(strlen(temp) + 1);
             strcpy(availableTerms[itemCount], temp);
             itemCount++;
         }
     }
 
-    // make sure enoguh terminals are open
+    // make sure enough terminals are open
     if (itemCount < 4) {
         printf("error: functionality requires at least 4 terminals total.\n");
+        exit(1);
     }
 
     availableTerms = realloc(availableTerms, itemCount*sizeof(char*));
 
-    printf("START DATE OF MISSION : %s", ctime(&t));
+    for (int i = 0; i < itemCount; i++){
+        fpt = fopen(availableTerms[i], "w");
+        fprintf(fpt, "START DATE OF MISSION : %s", ctime(&t));
+    }
 
     if (fork() != 0){
         if (fork() != 0){
             if (fork() != 0){
-
+                
             }
-            else 
+            else spawnSub(availableTerms[1]); 
         }
-        else
+        else spawnSub(availableTerms[2]);
     }
-    else 
-
-
+    else spawnSub(availableTerms[3]);
 
     return 0;
 }
+
+
+void spawnSub(char *Terminal) {
+    FILE* fptr = fopen(Terminal, "w");
+    
+    // initialize distance from base to 0
+    int distance = 0;
+
+    // initialize fuel between 1000 and 5000
+    int fuel = rand() % 5000 + 1000;
+
+    // initialize payload between 6 and 10 missiles
+    int payload = rand() % 10 + 6;
+
+
+    /*  use alarm() or setitimer and then:
+    
+        - decrement fuel by num between 100 and 200 each sec
+        - increment distance by 5 to 10 every two seconds if going towards
+            and decrements distance by 3 to 8 if returning to base
+        - reports military time, fuel amount, missiles left, and distance from base every 3 seconds
+    
+    */
+    
+    fclose(fptr);
+}
+
