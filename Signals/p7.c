2c0b67642b9f8d0194d666595007ed81c83dcad4
jagarwa1 <jaiveer.agarwal@gmail.com>
2023-04-27 00:01:46 -0400

    implemented random vars & clearing of consoles

--- Signals/p7.c
+++ Signals/p7.c
@@ -13,6 +13,9 @@ Known Bugs     -
 #include <unistd.h>
 #include <time.h>
 
+void clear(FILE* fpt) { fprintf(fpt, "\033[H\033[J"); }
+
+struct SUB{ int distance, fuel, payload;};
 
 // child operation
 void spawnSub(char *Terminal);
@@ -36,7 +39,7 @@ int main (int argc, char* arv[]) {
     // get opened terminals
     for (int i = 0; i <= 136; i++) {
         sprintf(temp, "/dev/pts/%d", i);
-        if (isatty(temp)) {
+        if (strcmp(temp, parentTTY) && fopen(temp, "r") != NULL) { // I want to keep parentTTY separately
             availableTerms[itemCount] = (char*)malloc(strlen(temp) + 1);
             strcpy(availableTerms[itemCount], temp);
             itemCount++;
@@ -44,44 +47,53 @@ int main (int argc, char* arv[]) {
     }
 
     // make sure enough terminals are open
-    if (itemCount < 4) {
+    if (itemCount < 3) {
         printf("error: functionality requires at least 4 terminals total.\n");
         exit(1);
     }
 
     availableTerms = realloc(availableTerms, itemCount*sizeof(char*));
 
-    for (int i = 0; i < itemCount; i++){
-        fpt = fopen(availableTerms[i], "w");
-        fprintf(fpt, "START DATE OF MISSION : %s", ctime(&t));
-    }
+    fpt = fopen(parentTTY, "w");
+    fprintf(fpt, "START DATE OF MISSION : %s\n", ctime(&t));
+
 
     if (fork() != 0){
         if (fork() != 0){
             if (fork() != 0){
                 
             }
-            else spawnSub(availableTerms[1]); 
+            else spawnSub(availableTerms[0]); 
         }
-        else spawnSub(availableTerms[2]);
+        else spawnSub(availableTerms[1]);
     }
-    else spawnSub(availableTerms[3]);
+    else spawnSub(availableTerms[2]);
 
     return 0;
 }
 
 
 void spawnSub(char *Terminal) {
-    FILE* fptr = fopen(Terminal, "w");
-    
+    struct SUB localSub;
+
+    FILE* fpt = fopen(Terminal, "w");
+    srand(time(0) + getpid());
+
+    clear(fpt);
+
     // initialize distance from base to 0
-    int distance = 0;
+    localSub.distance = 0;
 
     // initialize fuel between 1000 and 5000
-    int fuel = rand() % 5000 + 1000;
+    localSub.fuel = (rand() % (5000 - 1000 + 1)) + 1000;
 
     // initialize payload between 6 and 10 missiles
-    int payload = rand() % 10 + 6;
+    localSub.payload = (rand() % (10 - 6 + 1)) + 6;
+
+    fprintf(fpt, "I am local sub %d\n", getpid());
+    fprintf(fpt, "I am %d miles away from base\n", localSub.distance);
+    fprintf(fpt, "I have %d gallons of fuel\n", localSub.fuel);
+    fprintf(fpt, "I have %d ballistic missiles\n", localSub.payload);
 
 
     /*  use alarm() or setitimer and then:
@@ -89,10 +101,12 @@ void spawnSub(char *Terminal) {
         - decrement fuel by num between 100 and 200 each sec
         - increment distance by 5 to 10 every two seconds if going towards
             and decrements distance by 3 to 8 if returning to base
-        - reports military time, fuel amount, missiles left, and distance from base every 3 seconds
-    
+        - report military time, fuel amount, missiles left, and distance from base every 3 seconds
+            in each terminal
     */
+
+
     
-    fclose(fptr);
+    fclose(fpt);
 }
 
